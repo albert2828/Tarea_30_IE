6951e8c92b4d8586c266dcb79c772690baee771f
Alberto Macias <alberto.macbar9512@gmail.com>
2021-01-07 19:30:46 -0600

    inicio del análisis para la pregunta 3

--- tarea_30.r
+++ tarea_30.r
@@ -105,7 +105,7 @@ for (i in 2:10) {
 ## Pregunta 3
 ## Y si nos quedamos solo con los que fallecieron y hacemos un t-test entre las diferencias de los d?as?
 
-p3 <- tarea %>% select(FECHA_SINTOMAS, FECHA_INGRESO, FECHA_DEF) %>%
+p3 <- tarea %>% select(FECHA_SINTOMAS, FECHA_INGRESO, FECHA_DEF, DEFUNCION) %>%
             mutate(SINT_INGR = as.integer(FECHA_INGRESO - FECHA_SINTOMAS), 
                    SINT_FALLECE = as.integer(FECHA_DEF-FECHA_SINTOMAS))
 
@@ -141,9 +141,29 @@ p +
 
 lambda1 <- mean(p3i$SINT_INGR)
 lambda2 <- mean(p3f$SINT_FALLECE)
+sdi <- sd(p3i$SINT_INGR)
+sdf <- sd(p3f$SINT_FALLECE)
 
+## Queremos saber sin mientras más tiempo tarda una persona en acudir al hospital, más probabilidad de morir tiene
+p3_2 <- p3 %>% select(SINT_INGR, DEFUNCION) %>%
+            filter(SINT_INGR <= 20) %>%
+            mutate(fue_al_doctor = cut(SINT_INGR, breaks = c(-1,7,max(SINT_INGR)), 
+                                       labels = c("pronto","tarde")))
 
+table(p3_2[,c("SINT_INGR","DEFUNCION")])
+def_tarde.tble <- table(p3_2[,c("DEFUNCION", "fue_al_doctor")])   
+def_tarde.fisher.test <- fisher.test(def_tarde.tble)
+def_tarde.fisher.test
 
+prob_def_t.tets <- with(p3_2, t.test(DEFUNCION ~ fue_al_doctor, alt = "l"))
+prob_def_t.tets
+
+prob_def_t.tets <- with(p3_2, wilcox.test(DEFUNCION ~ fue_al_doctor, alt = "l"))
+prob_def_t.tets
 
 
 hist(p3$SINT_INGR, breaks = 30)
+
+
+## Calcular la tasa de positividad para la CDMX por alcaldia
+
